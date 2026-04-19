f67e3543d5983dc23f8ef6ef3bc8f72e0aa91277
qwoxic <deniszuravlev146@gmail.com>
2026-04-19 15:01:49 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I_Love_Team_Spirit'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lunar.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    display_name = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+@app.route('/')
+def index():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    return render_template('chat.html', user=session['display_name'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form.get('username')).first()
+        if user and check_password_hash(user.password_hash, request.form.get('password')):
+            session['user_id'] = user.id
+            session['display_name'] = user.display_name
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        display_name = request.form.get('display_name')
+        password = request.form.get('password')
+        
+        pw_hash = generate_password_hash(password) # сократил переменную
+        new_user = User(username=username, display_name=display_name, password_hash=pw_hash)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
